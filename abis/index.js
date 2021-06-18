862dda207e4bc2c5f0fb0ee9229027b802d1b14b
Jeff Wyckoff <jeff.wyckoff@ey.com>
2021-06-18 06:41:23 -0400

    Fourth Push Code working

--- abis/index.js
+++ abis/index.js
@@ -1,9 +1,11 @@
 const aaveLendingPool = require('./aave-lending-pool.json');
 const aaveDataProvider = require('./aave-data-provider.json');
+const aaveUiDataProvider = require('./aave-ui-data-provider.json');
 const aavePriceOracle = require('./aave-price-oracle.json');
 
 module.exports = {
   aaveLendingPool,
   aaveDataProvider,
+  aaveUiDataProvider,
   aavePriceOracle
 };
