c4667aba83da61836c4e428cb4f4011f31da68e7
Jeff Wyckoff <jeff.wyckoff@ey.com>
2021-06-21 15:05:11 -0400

    Updated CSVs

--- liquid-bot.js
+++ liquid-bot.js
@@ -9,7 +9,7 @@ const _ = require("lodash");
 const web3 = new Web3(new Web3(process.env.POLY_URL1));
 
 const { JsonRpcProvider } = require("@ethersproject/providers");
-const provider = new JsonRpcProvider("https://rpc-mainnet.matic.quiknode.pro",137);
+const provider = new JsonRpcProvider("https://apis.ankr.com/81906d60c883423cb783411d147b33ff/c9aade51b45f581467a524957c098b38/polygon/full/main",137);
 //console.log("Provider", provider);
 
 const aaveLendingPool = new web3.eth.Contract(
@@ -43,7 +43,7 @@ const init = async () => {
         //     fromBlock: block.number-1,
         //     toBlock: block.number
         //  }, function (error, events) { return events }),
-        aaveLendingPool.getPastEvents('Borrow', {
+        aaveLendingPool.getPastEvents('Deposit', {
             fromBlock: block.number-1,
             toBlock: block.number
          }, function (error, events) { return events }), 
@@ -60,6 +60,7 @@ const init = async () => {
     let userWethData;
     let userWbtcData;
     let userAaveData;
+    let borrowEventsFiltered;
 
     // userDaiData = (await aaveDataProvider.getReservesData("0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6","0x55a10618c7E9489ceE047705cD003df6d9e09195").call())
     // userUsdcData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.usdc.address,"0x55a10618c7E9489ceE047705cD003df6d9e09195").call())
@@ -74,14 +75,14 @@ const init = async () => {
     // console.log("AAVE: ",userAaveData);
 
     try {
-      borrowEvents = pastBorrows[0];
-      const borrowEventsFiltered = borrowEvents.filter( x => 
-        x.returnValues.reserve == addresses.reserves.dai.address || 
-        x.returnValues.reserve == addresses.reserves.usdc.address || 
-        x.returnValues.reserve == addresses.reserves.weth.address || 
-        x.returnValues.reserve == addresses.reserves.wbtc.address || 
-        x.returnValues.reserve == addresses.reserves.aave.address 
-        )
+      borrowEventsFiltered = pastBorrows[0];
+      // borrowEventsFiltered = borrowEvents.filter( x => 
+      //   x.returnValues.reserve == addresses.reserves.dai.address || 
+      //   x.returnValues.reserve == addresses.reserves.usdc.address || 
+      //   x.returnValues.reserve == addresses.reserves.weth.address || 
+      //   x.returnValues.reserve == addresses.reserves.wbtc.address || 
+      //   x.returnValues.reserve == addresses.reserves.aave.address 
+      //   )
       // console.log("Borrow Events: ",borrowEventsFiltered);
 
       var i;
@@ -89,8 +90,8 @@ const init = async () => {
         // we can get user or onBehalfOf, but onBehalfOf is the address that will be receiving the debt
         var userAddress = borrowEventsFiltered[i].returnValues.onBehalfOf;
         var reserveAddress = borrowEventsFiltered[i].returnValues.reserve;
-        console.log("User "+i+" Address: ",userAddress);
-        console.log("User "+i+" Reserve: ",reserveAddress);
+        // console.log("User "+i+" Address: ",userAddress);
+        // console.log("User "+i+" Reserve: ",reserveAddress);
 
         // getUserAccountData to get the healthFactor and ETH Position
         userData = (await aaveLendingPool.methods.getUserAccountData(userAddress).call())
@@ -115,41 +116,76 @@ const init = async () => {
           ethPositionFormatted = BigNumber(ethPosition).dividedBy(1e18).toFixed(2);
         } 
 
-        console.log("User "+i+" HealthFactor: ",healthyFormatted);
-        console.log("User "+i+" ethPostion: ",ethPositionFormatted);
+        if (healthyFormatted < 1 && healthyFormatted != null) {
+          console.log("User "+i+" Address: ",userAddress);
+          console.log("User "+i+" Reserve: ",reserveAddress);
+          console.log("User "+i+" HealthFactor: ",healthyFormatted);
+          console.log("User "+i+" ethPostion: ",ethPositionFormatted);
 
-        userDaiData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.dai.address,userAddress).call())
-        userUsdcData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.usdc.address,userAddress).call())
-        userWethData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.weth.address,userAddress).call())
-        userWbtcData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.wbtc.address,userAddress).call())
-        userAaveData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.aave.address,userAddress).call())
+          userDaiData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.dai.address,userAddress).call())
+          userUsdcData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.usdc.address,userAddress).call())
+          userWethData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.weth.address,userAddress).call())
+          userWbtcData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.wbtc.address,userAddress).call())
+          userAaveData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.aave.address,userAddress).call())
 
-        // console.log("DAI: ",userDaiData);
-        // console.log("USDC: ",userUsdcData);
-        // console.log("wETH: ",userWethData);
-        // console.log("wBTC: ",userWbtcData);
-        // console.log("AAVE: ",userAaveData);
-        
-        const tableData = [
-          ["userAddress", "reserveAddress", "currentATokenBalance", "usageAsCollateral?", "Bonus", "debtToCover", "debtReadable"],
-          [userAddress, addresses.reserves.dai.address, userDaiData.currentATokenBalance, userDaiData.usageAsCollateralEnabled, addresses.reserves.dai.reward, userDaiData.currentATokenBalance*addresses.reserves.dai.reward, BigNumber(userDaiData.currentATokenBalance).dividedBy(1e18)*0.5],
-          [userAddress, addresses.reserves.usdc.address, userUsdcData.currentATokenBalance, userUsdcData.usageAsCollateralEnabled, addresses.reserves.usdc.reward, userUsdcData.currentATokenBalance*addresses.reserves.usdc.reward, BigNumber(userUsdcData.currentATokenBalance).dividedBy(1e6)*0.5],
-          [userAddress, addresses.reserves.weth.address, userWethData.currentATokenBalance, userWethData.usageAsCollateralEnabled, addresses.reserves.weth.reward, userWethData.currentATokenBalance*addresses.reserves.weth.reward, BigNumber(userWethData.currentATokenBalance).dividedBy(1e18)*0.5],
-          [userAddress, addresses.reserves.wbtc.address, userWbtcData.currentATokenBalance, userWbtcData.usageAsCollateralEnabled, addresses.reserves.wbtc.reward, userWbtcData.currentATokenBalance*addresses.reserves.wbtc.reward, BigNumber(userWbtcData.currentATokenBalance).dividedBy(1e18)*0.5],
-          [userAddress, addresses.reserves.aave.address, userAaveData.currentATokenBalance, userAaveData.usageAsCollateralEnabled, addresses.reserves.aave.reward, userAaveData.currentATokenBalance*addresses.reserves.aave.reward, BigNumber(userAaveData.currentATokenBalance).dividedBy(1e18)*0.5]
-        ];
+          const [daiInEth, usdcInEth, wethInEth, wbtcInEth, aaveInEth] = (await aavePriceOracle.methods.getAssetsPrices([addresses.reserves.dai.address, addresses.reserves.usdc.address, addresses.reserves.weth.address, addresses.reserves.wbtc.address, addresses.reserves.aave.address]).call())
 
-        const keys = tableData.shift();
+          //console.log("Price Oracle: ",daiInEth);
+          
+          const tableData = [
+            ["userAddress", "reserveAddress", "currentATokenBalance", "usageAsCollateral?", "Bonus", "debtToCover" , "Profit", "GasCost"],
+            [ userAddress, 
+              addresses.reserves.dai.address, 
+              userDaiData.currentATokenBalance, 
+              userDaiData.usageAsCollateralEnabled, 
+              addresses.reserves.dai.reward, 
+              BigNumber(userDaiData.currentATokenBalance).dividedBy(1e18) * 0.5, 
+              BigNumber(userDaiData.currentATokenBalance).dividedBy(1e18) * 0.5 * addresses.reserves.dai.reward * BigNumber(daiInEth).dividedBy(1e18)],
+            [ userAddress, 
+              addresses.reserves.usdc.address, 
+              userUsdcData.currentATokenBalance, 
+              userUsdcData.usageAsCollateralEnabled, 
+              addresses.reserves.usdc.reward, 
+              BigNumber(userUsdcData.currentATokenBalance).dividedBy(1e6) * 0.5, 
+              BigNumber(userUsdcData.currentATokenBalance).dividedBy(1e6) * 0.5 * addresses.reserves.usdc.reward * BigNumber(usdcInEth).dividedBy(1e18)],
+            [ userAddress, 
+              addresses.reserves.weth.address, 
+              userWethData.currentATokenBalance, 
+              userWethData.usageAsCollateralEnabled, 
+              addresses.reserves.weth.reward, 
+              BigNumber(userWethData.currentATokenBalance).dividedBy(1e18) * 0.5, 
+              BigNumber(userWethData.currentATokenBalance).dividedBy(1e18) * 0.5 * addresses.reserves.weth.reward * BigNumber(wethInEth).dividedBy(1e18)],
+            [ userAddress, 
+              addresses.reserves.wbtc.address, 
+              userWbtcData.currentATokenBalance, 
+              userWbtcData.usageAsCollateralEnabled, 
+              addresses.reserves.wbtc.reward, 
+              BigNumber(userWbtcData.currentATokenBalance).dividedBy(1e18) * 0.5, 
+              BigNumber(userWbtcData.currentATokenBalance).dividedBy(1e18) * 0.5 * addresses.reserves.wbtc.reward * BigNumber(wbtcInEth).dividedBy(1e18)],
+            [ userAddress, 
+              addresses.reserves.aave.address, 
+              userAaveData.currentATokenBalance, 
+              userAaveData.usageAsCollateralEnabled, 
+              addresses.reserves.aave.reward, 
+              BigNumber(userAaveData.currentATokenBalance).dividedBy(1e18) * 0.5, 
+              BigNumber(userAaveData.currentATokenBalance).dividedBy(1e18) * 0.5 * addresses.reserves.aave.reward * BigNumber(aaveInEth).dividedBy(1e18)]
+          ];
 
-        const formatted = tableData.reduce((agg, arr) => {
-          agg.push(arr.reduce((obj, item, index) => {
-            obj[keys[index]] = item;
-            return obj;
-          }, {}));
-          return agg;
-        }, [])
+          const keys = tableData.shift();
 
-        console.log(formatted);
+          const formatted = tableData.reduce((agg, arr) => {
+            agg.push(arr.reduce((obj, item, index) => {
+              obj[keys[index]] = item;
+              return obj;
+            }, {}));
+            return agg;
+          }, [])
+
+          console.log(formatted);
+        }
+        else {
+          console.log("All Healthy");
+        }
         // if they have no ETH postion, print
         // if(ethPositionFormatted >= 1 && healthyFormatted < 1) {
         //   userReserveData = (await aaveDataProvider.methods.getUserReserveData(addresses.reserves.dai.address,userAddress).call())
