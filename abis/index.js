f76e650a4495b9f18a5d2ddd09f87a08b27b553d
Jeff Wyckoff <jeff.wyckoff@ey.com>
2021-07-07 00:40:21 -0400

    Added new poly contracts

--- abis/index.js
+++ abis/index.js
@@ -3,11 +3,15 @@ const aaveDataProvider = require('./aave-data-provider.json');
 const aaveUiDataProvider = require('./aave-ui-data-provider.json');
 const aavePriceOracle = require('./aave-price-oracle.json');
 const contractAbi = require('./contract-abi-2.json');
+const contractReserve = require('./contract-abi-3.json');
+const chainPrices = require('./chain-abi-prices.json');
 
 module.exports = {
   aaveLendingPool,
   aaveDataProvider,
   aaveUiDataProvider,
   aavePriceOracle,
-  contractAbi
+  contractAbi,
+  contractReserve,
+  chainPrices
 };
