363c13815079cfed6071cc9e0070136bd44409d8
matthias <mr3lucciola@gmail.com>
2021-07-19 12:26:40 -0400

    updated flashAndLiquidate.js abi; updated eventAnalyzer; updated liquidateAccounts for accurate liquidation calculations; removed clutter; started working on aaveEventListener

---
+++ src/looper.py
@@ -0,0 +1,14 @@
+from os import system
+from time import sleep, time
+# from datetime import datetime
+
+counter = 0
+timestart = time()
+while True:
+  loopstart = time()
+  counter += 1
+  system('yarn loop')
+  # dt = datetime.now()
+  print(f"\n loop count: {counter}  |||  time running: {time() - timestart} |||  loop time: {time() - loopstart} ")
+  # print('done')
+  sleep(60)
--- src/utils/web3Utils.js
+++ src/utils/web3Utils.js
@@ -1,22 +1,17 @@
-import Web3 from 'web3';
+// import Web3 from 'web3';
 
-export const getWeb3 = url => {
-  return new Web3(new Web3(url));
-};
+// export const getWeb3 = url => {
+//   return new Web3(new Web3(url));
+// };
 
-export const getContract = async (web3Instance, abi, address) => {
+export const getContract = (web3Instance, abi, address) => {
   let msg; 
-  if (!web3Instance) {
-    msg = `Please use valid web3Instance: ${web3Instance}`;
-  } else if (!abi) {
-    msg = `Please use valid abi: ${abi}`;
-  } else if (!address) {
-    msg = `Please use valid address: ${address}`;
-  }
-  if (msg) {
-    throw msg;
-  }
-  const contract = await new web3Instance.eth.Contract(abi, address);
+  if (!web3Instance) msg = `Please use valid web3Instance: ${web3Instance}`;
+  if (!abi) msg = `Please use valid abi: ${abi}`;
+  if (!address) msg = `Please use valid address: ${address}`;
+  if (msg) throw msg;
+
+  const contract = new web3Instance.eth.Contract(abi, address);
   return contract;
 };
 
@@ -24,16 +19,16 @@ export const getContract = async (web3Instance, abi, address) => {
  * @param {*} contractsObj - contains { abi (obj), address (str) } for all contracts
  * @returns 
  */
-export const getContracts = async (web3Instance, contractsObj) => {
-  const contractsArr = [];
-  for (const contractName of Object.keys(contractsObj)) {
-    const { abi, address } = contractsObj[contractName];
-    const contract = await getContract(web3Instance, abi, address);
-    contractsObj[contractName].contract = contract;
-    contractsArr.push({ key: contractName, ...contractsObj[contractName] });
-  }
-  return { contractsArr, contractsObj };
-};
+// export const getContracts = async (web3Instance, contractsObj) => {
+//   const contractsArr = [];
+//   for (const contractName of Object.keys(contractsObj)) {
+//     const { abi, address } = contractsObj[contractName];
+//     const contract = await getContract(web3Instance, abi, address);
+//     contractsObj[contractName].contract = contract;
+//     contractsArr.push({ key: contractName, ...contractsObj[contractName] });
+//   }
+//   return { contractsArr, contractsObj };
+// };
 
 export const closeWeb3 = web3Instance => {
   console.log('Closing web3 connection')
