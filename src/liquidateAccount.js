c598a957f798ec59a4b0bc751d3635a504b364f6
Jeff Wyckoff <jeff.wyckoff@ey.com>
2021-08-08 16:30:38 -0400

    Made some small changes

--- src/liquidateAccount.js
+++ src/liquidateAccount.js
@@ -285,3 +285,93 @@ export const liquidateSingleAccount = async (_accountObj, blockNumber) => {
     return;
   }
 };
+
+
+async function main() {
+  require('dotenv').config();
+  const { API_URL, PRIVATE_KEY } = process.env;
+  const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+  const web3 = createAlchemyWeb3(API_URL);
+  const myAddress = '0x610Ae88399fc1687FA7530Aac28eC2539c7d6d63' //TODO: replace this address with your own public address
+ 
+  const nonce = await web3.eth.getTransactionCount(myAddress, 'latest'); // nonce starts counting from 0
+
+  const transaction = {
+   'to': '0x31B98D14007bDEe637298086988A0bBd31184523', // faucet address to return eth
+   'value': 100,
+   'gas': 30000,
+   'maxFeePerGas': 1000000108,
+   'nonce': nonce,
+   // optional data field to send message or execute smart contract
+  };
+ 
+  const signedTx = await web3.eth.accounts.signTransaction(transaction, PRIVATE_KEY);
+  
+  web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+  if (!error) {
+    console.log("🎉 The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
+  } else {
+    console.log("❗Something went wrong while submitting your transaction:", error)
+  }
+ });
+}
+
+main();
+
+
+require("dotenv").config();
+const AlchemyWeb3 = require("@alch/alchemy-web3");
+
+const { API_URL_HTTP_PROD_RINKEBY, PRIVATE_KEY, ADDRESS } = process.env;
+const toAddress = "0x31B98D14007bDEe637298086988A0bBd31184523";
+const web3 = AlchemyWeb3.createAlchemyWeb3(API_URL_HTTP_PROD_RINKEBY);
+
+async function signTx(web3, fields = {}) {
+  const nonce = await web3.eth.getTransactionCount(ADDRESS, 'latest');
+
+  const transaction = {
+   'nonce': nonce,
+   ...fields,
+  };
+ 
+  return await web3.eth.accounts.signTransaction(transaction, PRIVATE_KEY);
+}
+
+async function sendTx(web3, fields = {}) {
+  const signedTx = await signTx(web3, fields);
+
+  web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+    if (!error) {
+      console.log("Transaction sent!", hash);
+      const interval = setInterval(function() {
+        console.log("Attempting to get transaction receipt...");
+        web3.eth.getTransactionReceipt(hash, function(err, rec) {
+          if (rec) {
+            console.log(rec);
+            clearInterval(interval);
+          }
+        });
+      }, 1000);
+    } else {
+      console.log("Something went wrong while submitting your transaction:", error);
+    }
+  });
+}
+
+function sendLegacyTx(web3) {
+  web3.eth.estimateGas({
+    to: toAddress,
+    data: "0xc6888fa10000000000000000000000000000000000000000000000000000000000000003"
+  }).then((estimatedGas) => {
+    web3.eth.getGasPrice().then((price) => {
+      sendTx(web3, {
+        gas: estimatedGas,
+        gasPrice: price,
+        to: toAddress,
+        value: 100,
+      });
+    });
+  });
+}
+
+sendLegacyTx(web3);
